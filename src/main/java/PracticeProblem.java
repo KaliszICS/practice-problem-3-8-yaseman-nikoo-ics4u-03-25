5d370b9513d018ee4ea2aa1de52282cf5164ee15
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-04-14 23:10:48 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,29 @@
+import java.util.*;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
+	public static int fib(int num){
+		HashMap<Integer,Integer> map= new HashMap<Integer,Integer>();
+		return fibHelper(num, map);
+	}
+	public static int fibHelper(int n, HashMap<Integer,Integer> map){
+		if(n==0){
+			map.put(n,0);
+			return 0;
+		}
+		if(n==1){
+			map.put(n,1);
+			return 1;
+		}
+		if(map.containsKey(n)){
+			return map.get(n);
+		}
+		int x = fibHelper(n-1, map)+ fibHelper(n-2, map);
+		map.put(n,x);
+		return x;
 
+	}
 	
 }
